fd4c10e6c5bbea288a65ace0b62e07f300504d1b
v1nw1n <54178250+v1nw1n@users.noreply.github.com>
2024-07-31 15:33:43 +0800

    Create nc_check_alive_status.sh

---
+++ nc_check_alive_status.sh
@@ -0,0 +1,35 @@
+#!/bin/zsh
+
+# target
+input_file="target.txt"
+# res
+result_file="ips_and_ports.txt"
+
+# file exist check
+if [[ ! -f "$input_file" ]]; then
+  echo "文件 $input_file 不存在"
+  exit 1
+fi
+
+echo "start..."
+while IFS=$' \t' read -r ip port; do
+  echo "[debug]readline: '$ip' '$port'"
+  # check ip and port
+  if [[ -n "$ip" && -n "$port" ]]; then
+    echo "[debug]connecting: $ip:$port"
+    # 捕获 nc 命令的输出
+    output=$(nc -z -v -G 3 "$ip" "$port" 2>&1)
+    echo "[debug]nc output: $output"
+    # check alive
+    if echo "$output" | grep -q "succeeded"; then
+      service=$(echo "$output" | sed -n 's/.*\[\(.*\)\].*/\1/p')
+      echo "$ip:$port:$service" >> "$result_file"
+    else
+      echo "[debug]----connect fail----: $ip $port"
+    fi
+  else
+    echo "[debug]format error:'$ip' '$port'"
+  fi
+done < "$input_file"
+
+echo "[debug]finish save res into: $result_file"
